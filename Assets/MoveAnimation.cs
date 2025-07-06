345f8b5aa11a6ddca38aa88167b37d168ef9246a
ProFenix309 <117487522+ProFenix309@users.noreply.github.com>
2025-07-06 14:27:22 -0500

    inicio

---
+++ Assets/MoveAnimation.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class MoveAnimation : MonoBehaviour
+{
+    private Animator animatorController;
+    [SerializeField] private float velocity;
+    [SerializeField] private float aceleration = 0.1f;
+    [SerializeField] private float deseleretion = 0.5f;
+
+    private void Awake()
+    {
+        animatorController = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        bool _move = Input.GetKey(KeyCode.W);
+        bool _Footins = Input.GetKey(KeyCode.J);
+
+        if (_move && velocity < 1)
+        {
+            velocity += Time.deltaTime * aceleration;
+            animatorController.SetFloat("Velocity", velocity);
+        }
+        if (!_move && velocity > 0)
+        {
+            velocity += Time.deltaTime * deseleretion;
+            animatorController.SetFloat("Velocity", velocity);
+        }
+
+        if (_Footins)
+        {
+            animatorController.SetBool("Comer", true);
+        }
+        if (!_Footins)
+        {
+            animatorController.SetBool("Comer", false);
+        }
+    }
+}
